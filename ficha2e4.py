4a8006e0dc4671f99e7541a15f479bf296120850
vasconeto <40240252@esmad.ipp.pt>
2024-10-17 12:02:34 +0100

    Ficha 2

--- ficha2e4.py
+++ ficha2e4.py
@@ -1,25 +1,28 @@
 import random
+while True:
+    num = random.randint(1,50)
+    n = int(input("Tentativa: "))
+    attem = 1
 
-num = random.randint(1,50)
-n = int(input("Tentativa: "))
-attem = 1
-
-while attem < 11 and n != num:
-    if n > num:
-        print("Numero e MENOR.")
-    elif n < num:
-        print("Numero e MAIOR.")
+    while attem < 11 and n != num:
+        if n > num:
+            print("Numero e MENOR.")
+        elif n < num:
+            print("Numero e MAIOR.")
     
-    attem += 1
-    if attem < 11:
-        n = int(input("Tentativa: "))
+        attem += 1
+        if attem < 11:
+            n = int(input("Tentativa: "))
     
-if n == num:
-    print("Acertou! Em %d tentativas" % (attem))
-else:
-    print("Esgotou as 10 tentativas :(")
+    if n == num:
+        print("Acertou! Em %d tentativas" % (attem))
+    else:
+        print("Esgotou as 10 tentativas :(")
 
-# new = input("Novo jogo (S/N)? ")
-# if new == "S":
+    new = input("Novo jogo (S/N)? ")
+    if new == "N":
+        break
+    elif new != "S":
+        print("Indique um valor correto")
     
     
